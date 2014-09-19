4687704c5e85473dbadd57f335a68c5df22882bf
marcelocaser <marcelocaser@b1925d01-8dd4-93ea-8c68-127e24e4def8>
2014-09-19 01:36:39 +0000

    Nova rotina de busca de CEP implementada.

---
+++ CEPGoB/README.markdown
@@ -0,0 +1,10 @@
+#Webservice de consulta a dados dos Correios
+
+## index.php - Webservice
+Imprime JSON da consulta de dados de CEP ou Código de Rastreio através de consultas com parametro GET (codigo_rastreio ou cep).
+
+## correios.class.php
+Classe que trata os dados dos Correios.
+
+## phpQuery-onefile.php
+Biblioteca utilizada para manipular html retornado pelos Correios.
--- CEPGoB/index.php
+++ CEPGoB/index.php
@@ -11,7 +11,15 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        header('Content-Type: text/html; charset=UTF-8');
+        include('correios.class.php');
+        if (isset($_GET['cep'])) {
+            die(json_encode(Correios::cep($_GET['cep'])));
+        } elseif (isset($_GET['codigo_rastreio'])) {
+            die(json_encode(Correios::rastreio($_GET['codigo_rastreio'])));
+        } else {
+            die('informe parametro GET cep ou codigo_rastreio');
+        }
         ?>
     </body>
 </html>
